127f99ff3a652d2bd4fe028d3a3d42d9ad65a432
Wolfgang Beer <wolfgaung.beer@gmail.com>
2017-08-12 14:56:44 +0200

    added computeModelMatrix()

--- src/Transform/Transform.spec.js
+++ src/Transform/Transform.spec.js
@@ -1,3 +1,4 @@
+import { quat, mat4 } from 'gl-matrix';
 import Transform from './';
 
 // ===================================================
@@ -10,16 +11,40 @@ test('it should have a position property', () => {
     expect(test.position).not.toBeUndefined();
 });
 
-test('it should have a scale property', () => {
+test('it should have a scaling property', () => {
     const test = new Transform();
-    expect(test).toHaveProperty('scale');
-    expect(test.scale).not.toBeUndefined();
+    expect(test).toHaveProperty('scaling');
+    expect(test.scaling).not.toBeUndefined();
 });
 
 test('it should have a quaternion property', () => {
     const test = new Transform();
     expect(test).toHaveProperty('quaternion');
     expect(test.quaternion).not.toBeUndefined();
+});
+
+test('it should have a positionDirty property', () => {
+    const test = new Transform();
+    expect(test).toHaveProperty('positionDirty');
+    expect(test.positionDirty).toEqual(false);
+});
+
+test('it should have a scalingDirty property', () => {
+    const test = new Transform();
+    expect(test).toHaveProperty('scalingDirty');
+    expect(test.scalingDirty).toEqual(false);
+});
+
+test('it should have a rotationDirty property', () => {
+    const test = new Transform();
+    expect(test).toHaveProperty('rotationDirty');
+    expect(test.rotationDirty).toEqual(false);
+});
+
+test('it should have a modelMatrix property', () => {
+    const test = new Transform();
+    expect(test).toHaveProperty('modelMatrix');
+    expect(test.modelMatrix).not.toBeUndefined();
 });
 
 // ===================================================
@@ -43,7 +68,7 @@ test('it should set the position property', () => {
     expect(test.getPosition()[2]).toEqual(3);
 });
 
-test('it should get the scale property', () => {
+test('it should get the scaling property', () => {
     const test = new Transform();
     expect(test.getScale()).not.toBeUndefined();
     expect(test.getScale()[0]).toEqual(1);
@@ -51,7 +76,7 @@ test('it should get the scale property', () => {
     expect(test.getScale()[2]).toEqual(1);
 });
 
-test('it should set the scale property', () => {
+test('it should set the scaling property', () => {
     const test = new Transform();
     test.setScale(1, 2, 3);
     expect(test.getScale()).not.toBeUndefined();
@@ -77,4 +102,246 @@ test('it should set the quaternion property', () => {
     expect(test.getQuaternion()[1]).toEqual(2);
     expect(test.getQuaternion()[2]).toEqual(3);
     expect(test.getQuaternion()[3]).toEqual(4);
+});
+
+test('it should get the modelMatrix property', () => {
+    const test = new Transform();
+    expect(test.getModelMatrix()).not.toBeUndefined();
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(0);
+    expect(test.getModelMatrix()[2]).toEqual(0);
+    expect(test.getModelMatrix()[3]).toEqual(0);
+});
+
+test('it should set the modelMatrix property', () => {
+    const test = new Transform();
+    test.setModelMatrix(mat4.fromValues(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1));
+    expect(test.getModelMatrix()).not.toBeUndefined();
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(1);
+    expect(test.getModelMatrix()[2]).toEqual(1);
+    expect(test.getModelMatrix()[3]).toEqual(1);
+});
+
+test('it should translate this Transform', () => {
+    const test = new Transform();
+    expect(test.getPosition()[0]).toEqual(0);
+    expect(test.getPosition()[1]).toEqual(0);
+    expect(test.getPosition()[2]).toEqual(0);
+    test.translate(1, 2, 3);
+    expect(test.getPosition()[0]).toEqual(1);
+    expect(test.getPosition()[1]).toEqual(2);
+    expect(test.getPosition()[2]).toEqual(3);
+    test.translate(2, 4, 6);
+    expect(test.getPosition()[0]).toEqual(3);
+    expect(test.getPosition()[1]).toEqual(6);
+    expect(test.getPosition()[2]).toEqual(9);
+    expect(test.positionDirty).toEqual(true);
+});
+
+test('it should scale this Transform', () => {
+    const test = new Transform();
+    expect(test.getScale()[0]).toEqual(1);
+    expect(test.getScale()[1]).toEqual(1);
+    expect(test.getScale()[2]).toEqual(1);
+    test.scale(1, 2, 3);
+    expect(test.getScale()[0]).toEqual(2);
+    expect(test.getScale()[1]).toEqual(3);
+    expect(test.getScale()[2]).toEqual(4);
+    test.scale(2, 4, 6);
+    expect(test.getScale()[0]).toEqual(4);
+    expect(test.getScale()[1]).toEqual(7);
+    expect(test.getScale()[2]).toEqual(10);
+    expect(test.scalingDirty).toEqual(true);
+});
+
+test('it should rotate this Transform', () => {
+    const test = new Transform();
+    const rotation1 = quat.fromEuler(quat.create(), 1, 2, 3);
+    expect(test.getQuaternion()[0]).toEqual(0);
+    expect(test.getQuaternion()[1]).toEqual(0);
+    expect(test.getQuaternion()[2]).toEqual(0);
+    expect(test.getQuaternion()[3]).toEqual(1);
+    test.rotate(1, 2, 3);
+    expect(test.getQuaternion()[0]).toEqual(rotation1[0]);
+    expect(test.getQuaternion()[1]).toEqual(rotation1[1]);
+    expect(test.getQuaternion()[2]).toEqual(rotation1[2]);
+    expect(test.getQuaternion()[3]).toEqual(rotation1[3]);
+    expect(test.rotationDirty).toEqual(true);
+});
+
+test('it should compute the modelMatrix (posDirty && sclDirty && rotDirty)', () => {
+    const test = new Transform();
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(0);
+    expect(test.getModelMatrix()[2]).toEqual(0);
+    expect(test.getModelMatrix()[3]).toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+    test.translate(1, 2, 3);
+    test.scale(2, 2, 2);
+    test.rotate(2, 4, 6);
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(0);
+    expect(test.getModelMatrix()[2]).toEqual(0);
+    expect(test.getModelMatrix()[3]).toEqual(0);
+    expect(test.positionDirty).toEqual(true);
+    expect(test.scalingDirty).toEqual(true);
+    expect(test.rotationDirty).toEqual(true);
+    test.computeModelMatrix();
+    expect(test.getModelMatrix()[0]).not.toEqual(1);
+    expect(test.getModelMatrix()[1]).not.toEqual(0);
+    expect(test.getModelMatrix()[2]).not.toEqual(0);
+    expect(test.getModelMatrix()[3]).toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+});
+
+test('it should compute the modelMatrix (posDirty && !sclDirty && !rotDirty)', () => {
+    const test = new Transform();
+    expect(test.getModelMatrix()[13]).toEqual(0);
+    expect(test.getModelMatrix()[14]).toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+    test.translate(1, 2, 3);
+    expect(test.getModelMatrix()[13]).toEqual(0);
+    expect(test.getModelMatrix()[14]).toEqual(0);
+    expect(test.positionDirty).toEqual(true);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+    test.computeModelMatrix();
+    expect(test.getModelMatrix()[13]).not.toEqual(0);
+    expect(test.getModelMatrix()[14]).not.toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+});
+
+test('it should compute the modelMatrix (!posDirty && sclDirty && !rotDirty)', () => {
+    const test = new Transform();
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[5]).toEqual(1);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+    test.scale(1, 2, 3);
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[5]).toEqual(1);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(true);
+    expect(test.rotationDirty).toEqual(false);
+    test.computeModelMatrix();
+    expect(test.getModelMatrix()[0]).not.toEqual(1);
+    expect(test.getModelMatrix()[5]).not.toEqual(1);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+});
+
+test('it should compute the modelMatrix (!posDirty && !sclDirty && rotDirty)', () => {
+    const test = new Transform();
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+    test.rotate(1, 2, 3);
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(true);
+    test.computeModelMatrix();
+    expect(test.getModelMatrix()[0]).not.toEqual(1);
+    expect(test.getModelMatrix()[1]).not.toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+});
+
+test('it should compute the modelMatrix (posDirty && sclDirty && !rotDirty)', () => {
+    const test = new Transform();
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[14]).toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+    test.translate(1, 2, 3);
+    test.scale(2, 2, 2);
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[14]).toEqual(0);
+    expect(test.positionDirty).toEqual(true);
+    expect(test.scalingDirty).toEqual(true);
+    expect(test.rotationDirty).toEqual(false);
+    test.computeModelMatrix();
+    expect(test.getModelMatrix()[0]).not.toEqual(1);
+    expect(test.getModelMatrix()[14]).not.toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+});
+
+test('it should compute the modelMatrix (posDirty && !sclDirty && rotDirty)', () => {
+    const test = new Transform();
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(0);
+    expect(test.getModelMatrix()[14]).toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+    test.translate(1, 2, 3);
+    test.rotate(2, 2, 2);
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(0);
+    expect(test.getModelMatrix()[14]).toEqual(0);
+    expect(test.positionDirty).toEqual(true);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(true);
+    test.computeModelMatrix();
+    expect(test.getModelMatrix()[0]).not.toEqual(1);
+    expect(test.getModelMatrix()[1]).not.toEqual(0);
+    expect(test.getModelMatrix()[14]).not.toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+});
+
+test('it should compute the modelMatrix (!posDirty && sclDirty && rotDirty)', () => {
+    const test = new Transform();
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+    test.scale(1, 2, 3);
+    test.rotate(2, 2, 2);
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(true);
+    expect(test.rotationDirty).toEqual(true);
+    test.computeModelMatrix();
+    expect(test.getModelMatrix()[0]).not.toEqual(1);
+    expect(test.getModelMatrix()[1]).not.toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+});
+
+test('it should compute the modelMatrix (!posDirty && !sclDirty && !rotDirty)', () => {
+    const test = new Transform();
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
+    test.computeModelMatrix();
+    expect(test.getModelMatrix()[0]).toEqual(1);
+    expect(test.getModelMatrix()[1]).toEqual(0);
+    expect(test.positionDirty).toEqual(false);
+    expect(test.scalingDirty).toEqual(false);
+    expect(test.rotationDirty).toEqual(false);
 });
--- src/Transform/index.js
+++ src/Transform/index.js
@@ -1,11 +1,15 @@
-import { vec3, quat } from 'gl-matrix';
+import { vec3, quat, mat4 } from 'gl-matrix';
 
 export default class Transform {
 
     constructor() {
         this.position = vec3.fromValues(0, 0, 0);
-        this.scale = vec3.fromValues(1, 1, 1);
+        this.scaling = vec3.fromValues(1, 1, 1);
         this.quaternion = quat.create();
+        this.positionDirty = false;
+        this.scalingDirty = false;
+        this.rotationDirty = false;
+        this.modelMatrix = mat4.create();
     }
 
     getPosition() {
@@ -17,11 +21,11 @@ export default class Transform {
     }
 
     getScale() {
-        return this.scale;
+        return this.scaling;
     }
 
     setScale(x, y, z) {
-        this.scale = vec3.fromValues(x, y, z);
+        this.scaling = vec3.fromValues(x, y, z);
     }
 
     getQuaternion() {
@@ -30,6 +34,85 @@ export default class Transform {
 
     setQuaternion(x, y, z, w) {
         this.quaternion = quat.fromValues(x, y, z, w);
+    }
+
+    getModelMatrix() {
+        return this.modelMatrix;
+    }
+
+    setModelMatrix(modelMatrix) {
+        this.modelMatrix = modelMatrix;
+    }
+
+    translate(x, y, z) {
+        this.position = vec3.add(vec3.create(), this.position, vec3.fromValues(x, y, z));
+        this.positionDirty = true;
+    }
+
+    scale(x, y, z) {
+        this.scaling = vec3.add(vec3.create(), this.scaling, vec3.fromValues(x, y, z));
+        this.scalingDirty = true;
+    }
+
+    rotate(x, y, z) {
+        const quaternion = quat.fromEuler(quat.create(), x, y, z);
+        this.quaternion = quat.multiply(quat.create(), this.quaternion, quaternion);
+        this.rotationDirty = true;
+    }
+
+    computeModelMatrix() {
+        const pos = this.position;
+        const scl = this.scaling;
+        const rot = this.quaternion;
+        const posDirty = this.positionDirty;
+        const sclDirty = this.scalingDirty;
+        const rotDirty = this.rotationDirty;
+
+        if (posDirty && sclDirty && rotDirty) { // everything changed
+
+            const transformationMatrix = mat4.fromRotationTranslationScale(mat4.create(), rot, pos, scl);
+            this.modelMatrix = mat4.multiply(mat4.create(), this.modelMatrix, transformationMatrix);
+
+        } else if (posDirty && !sclDirty && !rotDirty) { // only posDirty changed
+
+            const transformationMatrix = mat4.fromTranslation(mat4.create(), pos);
+            this.modelMatrix = mat4.multiply(mat4.create(), this.modelMatrix, transformationMatrix);
+
+        } else if (!posDirty && sclDirty && !rotDirty) { // only sclDirty changed
+
+            const transformationMatrix = mat4.fromScaling(mat4.create(), scl);
+            this.modelMatrix = mat4.multiply(mat4.create(), this.modelMatrix, transformationMatrix);
+
+        } else if (!posDirty && !sclDirty && rotDirty) { // only rotDirty changed
+
+            const transformationMatrix = mat4.fromQuat(mat4.create(), rot);
+            this.modelMatrix = mat4.multiply(mat4.create(), this.modelMatrix, transformationMatrix);
+
+        } else if (posDirty && sclDirty && !rotDirty) { // posDirty and sclDirty changed
+
+            const posMatrix = mat4.fromTranslation(mat4.create(), pos);
+            const sclMatrix = mat4.fromScaling(mat4.create(), scl);
+            this.modelMatrix = mat4.multiply(mat4.create(), this.modelMatrix, sclMatrix);
+            this.modelMatrix = mat4.multiply(mat4.create(), this.modelMatrix, posMatrix);
+
+
+        } else if (posDirty && !sclDirty && rotDirty) { // posDirty and rotDirty changed
+
+            const transformationMatrix = mat4.fromRotationTranslation(mat4.create(), rot, pos);
+            this.modelMatrix = mat4.multiply(mat4.create(), this.modelMatrix, transformationMatrix);
+
+        } else if (!posDirty && sclDirty && rotDirty) { // sclDirty and rotDirty changed
+
+            const sclMatrix = mat4.fromScaling(mat4.create(), scl);
+            const rotMatrix = mat4.fromQuat(mat4.create(), rot);
+            this.modelMatrix = mat4.multiply(mat4.create(), this.modelMatrix, sclMatrix);
+            this.modelMatrix = mat4.multiply(mat4.create(), this.modelMatrix, rotMatrix);
+
+        }
+
+        this.positionDirty = false;
+        this.scalingDirty = false;
+        this.rotationDirty = false;
     }
 
 }
